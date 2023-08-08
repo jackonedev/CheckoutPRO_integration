b1e67b5b75aa3dd3f13fe93edd978bc0f7bd00bb
unknown <jackone.action.software@gmail.com>
2023-08-08 16:56:01 -0300

    updating repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,32 @@
 import React, { useState } from "react";
 import { initMercadoPago } from "@mercadopago/sdk-react";
 import InternalProvider from "./components/ContextProvider";
-import Payment from "./components/Payment";
-import Checkout from "./components/Checkout";
-import Footer from "./components/Footer";
+import Payment from "./components/pure/Payment";
+import Checkout from "./components/pure/Checkout";
+import Footer from "./components/pure/Footer";
 import { SpinnerCircular } from "spinners-react";
-// import { URL, ACCESS_TOKEN, PUBLIC_KEY } from "./config.js";
+import FormContainer from "./components/container/FormContainer";
 
-initMercadoPago("APP_USR-d6b54237-cca0-490f-900a-59405851aa7b");
+initMercadoPago("APP_USR-ff96fe80-6866-4888-847e-c69250754d38");
 
-function App() {
-  const backend_url = "http://localhost:8000";
+function OrderHandling({ sendData, setPreferenceId }) {
+  const PORT = 8000;
+  const HOST = "localhost";
+  const URL = `http://${HOST}:${PORT}`;
 
-  const item = {
-    quantity: 1,
-    price: "10",
-    amount: 10,
-    description: "Some book",
-  };
-
-  const [preferenceId, setPreferenceId] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [orderData, setOrderData] = useState(item);
 
   const handleClick = () => {
+    console.log("sendData: ", sendData);
     setIsLoading(true);
-    fetch(backend_url + "/create_preference", {
+    fetch(URL + "/v1/create_preference", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
-      body: JSON.stringify(orderData),
+      body: JSON.stringify(sendData),
     })
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((preference) => {
         setPreferenceId(preference.id);
       })
@@ -58,13 +49,56 @@ function App() {
   };
 
   return (
+    <div>
+      {renderSpinner()}
+      <Checkout onClick={handleClick} params={sendData.orderData} />
+    </div>
+  );
+}
+
+function App() {
+  const item = {
+    quantity: 1,
+    price: "5000",
+    amount: 5000,
+    description: "Mobile Phone",
+    img_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/DynaTAC8000X.jpg/220px-DynaTAC8000X.jpg",
+    name: "Motorola DynaTAC 8000X",
+  };
+  const client = {
+    nombre_apellido: "Lalo Landa",
+    email: "test_user_36961754@testuser.com",
+    telefono: "3516475847",
+    direccion: "calle falsa 123",
+    codigo_postal: "5000",
+  };
+
+  const [preferenceId, setPreferenceId] = useState(null);
+  const [orderData, setOrderData] = useState(item);
+  const [formDataCliente, setFormDataCliente] = useState(client);
+
+  const combinedData = {
+    orderData: orderData,
+    formDataCliente: formDataCliente,
+  };
+  return (
     <InternalProvider
-      context={{ preferenceId, isLoading, orderData, setOrderData }}
+      context={{
+        preferenceId,
+        orderData,
+        setOrderData,
+        formDataCliente,
+        setFormDataCliente,
+      }}
     >
       <main>
-        {renderSpinner()}
-        <Checkout onClick={handleClick} params={item} />
+        <OrderHandling
+          sendData={combinedData}
+          setPreferenceId={setPreferenceId}
+        />
         <Payment />
+        <FormContainer />
       </main>
       <Footer />
     </InternalProvider>
--- server/main.py
+++ server/main.py
@@ -2,20 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-import mercadopago
-from dotenv import load_dotenv
-import os
 from fastapi.responses import FileResponse
 
+import api
 
-load_dotenv()
 
 
 app = FastAPI()
 
 #  STATIC FILES
-app.mount("/assets", StaticFiles(directory="dist/assets"), name="assets")
-app.mount("/img", StaticFiles(directory="dist/assets/img"), name="img")
+# app.mount("/static", StaticFiles(directory="./frontend/static"), name="static")
+# app.mount("/img", StaticFiles(directory="./frontend/static/img"), name="img")
 
 
 # CORS
@@ -33,50 +30,10 @@ app.add_middleware(
 )
 
 
-# PATH OPERATION FUNCTIONS
-@app.post("/create_preference")
-async def create_preference(request: Request):
-    body = await request.json()
-
-    # # creating SDK instance
-    # access_token = request.headers["Authorization"].split(" ")[1]
-    access_token = os.getenv("ACCESS_TOKEN")
-    sdk = mercadopago.SDK(access_token)
-
-    preference = {
-        "items": [
-            {
-                "title": body["description"],
-                "unit_price": float(body["price"]),
-                "quantity": int(body["quantity"]),
-            }
-        ],
-        "back_urls": {
-            "success": "http://localhost:8000/feedback",
-            "failure": "http://localhost:8000/feedback",
-            "pending": "http://localhost:8000/feedback"
-        },
-        "auto_return": "approved",
-    }
-    response = sdk.preference().create(preference)
-    if response["status"] == 201:
-        return JSONResponse(content={"id": response["response"]["id"]})
-    else:
-        print(f"\tMercadoPago response error:{response}")
-        return JSONResponse(content={"error": response["response"]})
-
-
-@app.get('/feedback')
-async def feedback(request: Request, collection_id: str, collection_status: str, payment_id: str, status: str, external_reference: str, payment_type: str, merchant_order_id: str, preference_id: str, site_id: str, processing_mode: str, merchant_account_id: str):
-    # print(f"request: {request}") -> <starlette.requests.Request object at 0x000002057C96AA90>
-    return JSONResponse(content={
-        "Payment": payment_id,
-        "Status": status,
-        "MerchantOrder": merchant_order_id
-    })
-
+app.include_router(api.router)
 
 @app.get("/")
 async def read_root():
-    # Devolver el archivo HTML principal de React
-    return FileResponse("dist/index.html")
+    return {"Hello": "World"}
+#     # Devolver el archivo HTML principal de React
+#     return FileResponse("./frontend/index.html")
