7374cb91ed3047386fa2ce265564742b28e54ac9
unknown <jackone.action.software@gmail.com>
2023-08-09 16:10:40 -0300

    comment added

--- server/preference_validation.py
+++ server/preference_validation.py
@@ -66,7 +66,7 @@ class Preferencia(BaseModel):
     auto_return: str = "all"
     payment_methods: PaymentMethods
     notification_url: HttpUrl
-    statement_descriptor: Optional[str]
+    statement_descriptor: Optional[str] # Nombre del emisor de la factura
     external_reference: str
     expires: bool = True
     expiration_date_from: datetime = datetime.now()
